a249dcd5bb692648bf271d15696fd4489c31b006
pratham2609 <pratham1055.be21@chitkara.edu.in>
2023-08-25 14:34:59 +0530

    initial commit of spotify clone

---
+++ components/sidebar.tsx
@@ -0,0 +1,15 @@
+'use client'
+interface SidebarProps {
+    children: React.ReactNode
+}
+import React from 'react'
+
+
+const Sidebar: React.FC<SidebarProps> = ({
+    children
+}) => {
+    return (
+        <div></div>
+    )
+}
+export default Sidebar;
