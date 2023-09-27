daa04d4147ee869d82dabb8e1a2d7ee26cdd59d8
Pratham Mehta <pratham1055.be21@chitkara.edu.in>
2023-09-27 11:08:00 +0530

    search and liked songs functionality done

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import LikeButton from "@/components/LikeButton";
+import MediaItem from "@/components/MediaItem";
+import { Song } from "@/types";
+
+interface SearchContentProps {
+    songs: Song[];
+}
+const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+    if (songs.length == 0 || !songs) {
+        return <div className="text-neutral-400 w-full px-6 gap-y-2 flex flex-col text-3xl">
+            No Songs Found
+        </div>
+    }
+    return (<section className="flex flex-col gap-y-2 w-full px-6">
+        {songs.map((song) => (
+            <div key={song.id} className="flex items-center gap-x-4 w-full">
+                <div className="flex-1">
+                    <MediaItem onClick={() => { }} data={song} />
+                </div>
+                <LikeButton songId={song.id} />
+            </div>
+        ))}
+    </section>);
+}
+
+export default SearchContent;
